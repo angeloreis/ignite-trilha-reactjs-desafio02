455287769bc982e42624d7ec9358fb4dfc4a48e5
Angelo Reis <angelo.reis@kumulus.com.br>
2021-08-27 19:24:51 -0300

    feat: implementando components

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,31 @@
-export function SideBar() {
-  // Complete aqui
+import {Button} from '../components/Button'
+import { genre } from '../services/types'
+
+import '../styles/sidebar.scss';
+
+interface SideBarProps {
+  genres: genre[];
+  eventClickButton(id: number): void;
+  selectedGenreId: number;
 }
+
+export function SideBar({genres, eventClickButton, selectedGenreId}: SideBarProps) {
+  return (
+    <nav className="sidebar">
+    <span>Watch<p>Me</p></span>
+
+    <div className="buttons-container">
+      {genres.map(genre => (
+        <Button
+          key={String(genre.id)}
+          title={genre.title}
+          iconName={genre.name}
+          onClick={() => eventClickButton(genre.id)}
+          selected={selectedGenreId === genre.id}
+        />
+      ))}
+    </div>
+  </nav>
+  );
+}
+
